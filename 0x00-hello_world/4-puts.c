a24171dcf1be3354970221db3fbd078c5e2ae7fe
justin-ipe <justinmwaipe@gmail.com>
2023-02-18 04:54:10 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming islike building a multilingual puzzle");
+	return (0);
+}
