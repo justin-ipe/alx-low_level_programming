665f677bc90ae2493b518d0f528233b83329d7e6
justin-ipe <justinmwaipe@gmail.com>
2023-03-08 19:18:09 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,28 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion - function declaration
- * @s: the string
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-
-void _puts_recursion(char *s);
-
-/**
- * _puts_recursion - function definition
- * @s: thr string to recurse
- * Description: recurses through a string s
- * Return: void
- */
-
 void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(++s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	{
 		_putchar('\n');
-	}
 }
