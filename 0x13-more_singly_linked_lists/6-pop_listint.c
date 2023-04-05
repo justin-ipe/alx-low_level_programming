2f3cb7b3f3ff12373508edf40384ca86a94128e0
justin-ipe <justinmwaipe@gmail.com>
2023-04-05 13:52:59 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a list
+ * @head: pointer to list
+ */
+ int pop_listint(list_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	temp = *head;
+
+	if (temp == NULL)
+		return (0);
+	*head = temp->next;
+
+	n = temp->;
+	free(temp);
+	return (n);
+}
