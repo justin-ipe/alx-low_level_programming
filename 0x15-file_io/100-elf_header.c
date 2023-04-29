194c999c4e102e8649f2f3982df0477c4f25c11d
justin-ipe <justinmwaipe@gmail.com>
2023-04-29 13:52:44 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -175,7 +175,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		closeelf(o);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
@@ -183,7 +183,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (r == -1)
 	{
 		free(header);
-		close_elf(o);
+		closeelf(o);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -192,15 +192,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
-	print_data(header->e_ident);
+	data(header->e_ident);
 	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
+	printosabi(header->e_ident);
+	printbi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
+	entry_print(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	closeelf(o);
 	return (0);
 }
 
