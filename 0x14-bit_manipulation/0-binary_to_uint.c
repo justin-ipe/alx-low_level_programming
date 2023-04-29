6c51fc8e87641103dfdbb633818c11f372d20d80
justin-ipe <justinmwaipe@gmail.com>
2023-04-29 12:44:08 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,41 +10,86 @@ int str_len(char *str);
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	unsigned int j = 1;
-	int len;
+	char *s = _revstr(b); /* reverse string for easier parsing */
+	unsigned int i, j, mul, b_digit, result = 0;
 
-	len = str_len(b) - 1;
-	while (len >= 0)
+	/* check for NULL pointer */
+	if (!s)
+		return (0);
+
+	for (i = 0; s[i]; i++)
 	{
-		if (b[len] == '1')
+		/* check if valid binary char */
+		if (s[i] != '0' && s[i] != '1')
 		{
-			n += j;
-		}
-		j = j * 2;
-		if (b[len] != '1' && b[len] != '0')
-		{
+			free(s);
 			return (0);
 		}
-		len--;
+
+		/* convert binary char to int */
+		b_digit = s[i] - '0';
+
+		/* if index is greater than 0 */
+		if (i)
+		{
+			/* loop to produce 2 to the ith power */
+			for (j = 0, mul = 1; j < i; j++)
+				mul *= 2;
+			/* multiply binary digit by result of 2^i */
+			result += b_digit * mul;
+		}
+		/* if index is 0 (i.e. 2^0) */
+		else
+			result += b_digit;
 	}
-	if (b == NULL)
-		return (0);
-	return (n);
+	free(s);
+	return (result);
 }
-
 /**
-* str_len - Finds the length of a string
-* @str: String length of is found
+* _revstr - reverses a constant string
+* @src: string to be reversed
 *
-* Return: length of the @str
+* Return: reversed string
 */
-
-int str_len(char *str)
+char *_revstr(const char *src)
 {
-	int l = 0;
+	int i, j; /* looping variables */
+	char *dest; /* target string */
 
-	while (str[l] != '\0')
-		l++;
-	return (l);
+	/* if NULL, return "(null)" */
+	if (!src)
+		return (NULL);
+
+	/* allocate memory for dest */
+	dest = malloc(sizeof(char) * (_strlen(src) + 1));
+	if (dest == NULL)
+		return (NULL);
+
+	/* copy arg to new string in reverse */
+	i = _strlen(src) - 1;
+	j = 0;
+	while (i >= 0)
+		dest[j++] = src[i--];
+
+	/* initialize null-byte */
+	dest[j] = '\0';
+
+	return (dest);
+}
+/**
+* _strlen - Function that finds the length of a string
+* @s: string that the length of is found
+*
+* Return: length of the @s
+*/
+int _strlen(const char *s)
+{
+	/* variable to loop through string to get length */
+	int len = 0;
+
+	/* Loop through string, checking for null byte */
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
 }
