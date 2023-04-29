23a6af453b6663f57a2510037c4f03b270637019
justin-ipe <justinmwaipe@gmail.com>
2023-04-29 06:35:36 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_text_file(const char *namefile, size_t num_letters);
+
+#endif
