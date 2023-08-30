3343d059cd36fe237773bcf33149a343e0a85612
justin-ipe <justinmwaipe@gmail.com>
2023-08-30 12:41:13 -0700

    task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 95 && c <= 120)
+	{
+	return (1);
+	}
+	return (0);
+}
